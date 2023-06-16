4a9a23c29e62094a3db21f76e7d3cca7f6e9de70
NisalaRV <nisalaravihara129@gmail.com>
2023-06-16 22:03:32 +0530

    added  update and delete button to inMemoryPos

--- assignments/JS/InMemoryPOS/SPA/Js/Customer.js
+++ assignments/JS/InMemoryPOS/SPA/Js/Customer.js
@@ -20,7 +20,6 @@ $("#add").click(function () {
     customerDB.push(customerOb);
 
 
-
 });
 
 
@@ -61,3 +60,163 @@ $("#tblCustomer").on('click', 'tr', function() {
     $("#customerBirthday").val(date);
 
 });
+
+function deleteCustomer(id){
+    for (let i = 0; i < customerDB.length; i++) {
+        if(customerDB[i].id==id){
+            customerDB.splice(i,1);
+            return true;
+        }
+    }
+    return false
+}
+
+$("#btnDelete").click(function () {
+    let id=$("#CustomerID").val();
+    let consent=confirm("Do You Want to delete ");
+    if(consent){
+        let response = deleteCustomer(id);
+        if(response){
+            alert("customer deleted")
+
+        }else {
+            alert("customer not deleted")
+        }
+    }
+
+
+});
+
+
+$("#UpdateCustomer").click(function () {
+
+    let id = $("#CustomerID").val();
+
+    confirm("DO you want update this customer")
+    let response = updateCustomer(id);
+
+    if(response){
+        alert("Customer update sucess")
+
+    }else {
+        alert("Somethin went wrong")
+    }
+});
+
+function updateCustomer(id) {
+    for (let i=0;i<customerDB.length;i++){
+        if(customerDB[i].id==id){
+
+            let name=$("#customerName").val();
+            let address=$("#customerAddress").val();
+            let date=$("#customerBirthday").val();
+            customerDB[i].name=name;
+            customerDB[i].address=address;
+            customerDB[i].date=date;
+        }
+        return true
+    }
+}
+
+function updateCustomer(id){
+    if (searchCustomer(id)==undefined) {
+        alert("No such Customer..please check the ID");
+    }else{
+        let consent= confirm("Do you really want to update this customer.?");
+        if (consent) {
+            let customer= searchCustomer(id);
+
+            let customerName = $("#customerName").val();
+            let customerAddress = $("#customerAddress").val();
+            let customerBirthday = $("#customerBirthday").val();
+
+            customer.name=customerName;
+            customer.address=customerAddress;
+            customer.date=customerBirthday;
+
+            getAllCustomers();
+        }
+    }
+
+}
+function searchCustomer(id){
+    return customerDB.find(function (customer){
+        //if the search id match with customer record
+        //then return that object
+        return  customer.id==id;
+    });
+}
+
+$("#CustomerID").keydown(function (e){
+    if (e.key=="Enter"){
+        $("#customerName").focus();
+    }
+})
+
+$("#customerName").keydown(function (e){
+    if (e.key=="Enter"){
+        $("#customerAddress").focus();
+    }
+})
+$("#customerAddress").keydown(function (e){
+    if (e.key=="Enter"){
+        $("#customerBirthday").focus();
+    }
+})
+$("#customerBirthday").keydown(function (e){
+    if (e.key=="Enter"){
+        $("#CustomerID").focus();
+    }
+})
+
+
+$("#CustomerID").keyup(function (e) {
+    const pattern = /^(C00-)[0-9]{3}$/;
+    if (pattern.test($("#CustomerID").val())){
+        $("#CustomerID").css('border','2px solid green');
+    }else{
+        $("#CustomerID").css('border','2px solid red')
+        if (e.key==='Enter'){
+            console.log(e);
+            e.preventDefault();
+        }
+    }
+});
+$("#customerName").keyup(function (e) {
+    const pattern = /^[a-zA-Z\s']+$/u;
+    if (pattern.test($("#customerName").val())){
+        $("#customerName").css('border','2px solid green');
+    }else{
+        $("#customerName").css('border','2px solid red')
+        if (e.key==='Enter'){
+            console.log(e);
+            e.preventDefault();
+        }
+    }
+});
+$("#customerAddress").keyup(function (e) {
+    const pattern =  /^[A-z ,.0-9]{3,}$/u
+    ;
+    if (pattern.test($("#customerAddress").val())){
+        $("#customerAddress").css('border','2px solid green');
+    }else{
+        $("#customerAddress").css('border','2px solid red')
+        if (e.key==='Enter'){
+            console.log(e);
+            e.preventDefault();
+        }
+    }
+});
+$("#customerBirthday").keyup(function (e) {
+    const pattern =/^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/ ;
+    if (pattern.test($("#customerBirthday").val())){
+        $("#customerBirthday").css('border','2px solid green');
+    }else{
+        $("#customerBirthday").css('border','2px solid red')
+        if (e.key==='Enter'){
+            console.log(e);
+            e.preventDefault();
+        }
+    }
+});
+
