49211e2f8fe1122abeb336ab1a8a57bf63d56365
NisalaRV <nisalaravihara129@gmail.com>
2023-06-20 02:01:07 +0530

    added orderjs to inMemoryPOS

--- assignments/JS/InMemoryPOS/SPA/Js/Order.js
+++ assignments/JS/InMemoryPOS/SPA/Js/Order.js
@@ -1,9 +1,9 @@
 var orderDB = [];
 
-$("#orderAdd").click(function () {
+function addOrder(){
 
     let OrderId = $("#OrderID1").val();
-    let Item_code = $("#Itemcode1").val();
+    let Item_code1 = $("#Itemcode1").val();
     let date = $("#itemDate").val();
     let price=$("#OPrice").val();
     let Qty=$("#oQty").val();
@@ -11,7 +11,7 @@ $("#orderAdd").click(function () {
 
     let orderOb = {
         id:OrderId,
-        code:Item_code,
+        code:Item_code1,
         date:date,
         price:price,
         qty: Qty
@@ -19,24 +19,29 @@ $("#orderAdd").click(function () {
 
     orderDB.push(orderOb);
 
+    getAllOrders();
 
+}
 
+$("#orderAdd").click(function () {
+    addOrder();
 });
 
 
-$("#getAllOrder").click(function () {
-
-    $("#tblOrder").empty();
 
 
-    for (let i = 0; i < orderDB.length; i++) {
-        let id = orderDB[i].id;
-        let code = orderDB[i].code;
-        let date = orderDB[i].date;
-        let price = orderDB[i].price;
-        let  qty= orderDB[i].qty;
+    function getAllOrder() {
+        $("#tblOrder").empty();
 
-        let row =`<tr>
+
+        for (let i = 0; i < orderDB.length; i++) {
+            let id = orderDB[i].id;
+            let code = orderDB[i].code;
+            let date = orderDB[i].date;
+            let price = orderDB[i].price;
+            let qty = orderDB[i].qty;
+
+            let row = `<tr>
                      <td>${id}</td>
                      <td>${code}</td>
                      <td>${date}</td>
@@ -45,27 +50,35 @@ $("#getAllOrder").click(function () {
 
                     <tr>`
 
-        $("#tblOrder").append(row);
+            $("#tblOrder").append(row);
 
+            bindTrEvent()
+        }
     }
 
-});
-$("#tblOrder").on('click', 'tr', function() {
+    $("#getAllOrder").click(function (){
+        getAllOrder();
+    });
 
-    let id = $(this).children(":eq(0)").text();
-    let code = $(this).children(":eq(1)").text();
-    let date = $(this).children(":eq(2)").text();
-    let price = $(this).children(":eq(3)").text();
-    let qty = $(this).children(":eq(4)").text();
 
-    $("#OrderID1").val(id);
-    $("#Itemcode1").val(code);
-    $("#itemDate").val(date);
-    $("#OPrice").val(price);
-    $("#oQty").val(qty);
+function bindTrEvent() {
 
-});
+    $('#tblOrder>tr').click(function (){
 
+        let id = $(this).children(":eq(0)").text();
+        let code = $(this).children(":eq(1)").text();
+        let date = $(this).children(":eq(2)").text();
+        let price = $(this).children(":eq(3)").text();
+        let qty = $(this).children(":eq(4)").text();
+
+        $("#OrderID1").val(id);
+        $("#Itemcode1").val(code);
+        $("#itemDate").val(date);
+        $("#OPrice").val(price);
+        $("#oQty").val(qty);
+
+    })
+}
 // _____________
 
 
@@ -97,36 +110,179 @@ $("#orderdelete").click(function () {
 
 // // -----------update------------
 
+$("#orderupdate").click(function (){
+    let id= $("#OrderID1").val();
+    updateOrder(id);
 
-$("#orderupdate").click(function () {
+    clearOrderInputFields();
+});
 
-    let id = $("#OrderID1").val();
 
-    confirm("DO you want update this item")
-    let response = updateItem(id);
+function updateOrder(id){
+    if (searchOrder(id)==undefined) {
+        alert("No such Order..please check the ID");
+    }else{
+        let consent= confirm("Do you really want to update this Order.?");
+        if (consent) {
+            let order= searchOrder(id);
+            //if the customer available can we update.?
 
-    if(response){
-        alert("item update sucess")
+            let code = $("#Itemcode1").val();
+            let date = $("#itemDate").val();
+            let price = $("#OPrice").val();
+            let qty = $("#oQty").val();
 
-    }else {
-        alert("Somethin went wrong")
+            order.code=code;
+            order.date=date;
+            order.price=price;
+            order.qty=qty;
+
+            getAllOrders();
+        }
+    }
+
+}
+function searchOrder(id){
+    return orderDB.find(function (order){
+        return order.id==id;
+    });
+}
+function clearOrderInputFields() {
+    $("#OrderID1,#Itemcode1,#itemDate,#OPrice,#oQty").val("");
+    $("#OrderID1").focus();
+}
+
+$("#OrderID1,#Itemcode1,#itemDate,#OPrice,#oQty").keydown(function (e) {
+
+    if (e.key == "Tab") {
+        e.preventDefault();
     }
 });
 
-function updateOder(id) {
-    for (let i=0;i<orderDB.length;i++){
-        if(orderDB[i].id==id){
+$("#OrderID1").keydown(function (e){
 
-            let code=$("#Itemcode1").val();
-            let date=$("#itemDate").val();
-            let price=$("#OPrice").val();
-            let qty=$("#oQty").val();
-            orderDB[i].code=code;
-            orderDB[i].date=date;
-            orderDB[i].price=price;
-            orderDB[i].qty=qty;
-        }
-        return true
+    if (e.key=="Enter"){
+        $("#Itemcode1").focus();
     }
-}
 
+});
+$("#Itemcode1").keydown(function (e){
+
+    if (e.key=="Enter"){
+        $("#itemDate").focus();
+    }
+
+});
+
+$("#itemDate").keydown(function (e){
+
+    if (e.key=="Enter"){
+        $("#OPrice").focus();
+    }
+
+});
+$("#OPrice").keydown(function (e){
+
+    if (e.key=="Enter"){
+        $("#oQty").focus();
+    }
+
+});
+
+$("#oQty").keydown(function (e){
+    if (e.key=="Enter"){
+        addOrder();
+        clearOrderInputFields();
+    }
+});
+
+
+$("#OrderID1").keyup(function (e) {
+    const pattern = /^(O00)[0-9]$/;
+    if (pattern.test($("#OrderID1").val())){
+        $("#OrderID1").css('border','2px solid green');
+
+
+    }else{
+        $("#OrderID1").css('border','2px solid red')
+        if (e.key==='Enter'){
+            console.log(e);
+            e.preventDefault();
+
+        }
+
+    }
+
+});
+
+$("#Itemcode1").keyup(function (e) {
+    const pattern = /^(I00)[0-9]$/;
+    if (pattern.test($("#Itemcode1").val())){
+        $("#Itemcode1").css('border','2px solid green');
+
+
+    }else{
+        $("#Itemcode1").css('border','2px solid red')
+        if (e.key==='Enter'){
+            console.log(e);
+            e.preventDefault();
+
+        }
+
+    }
+
+});
+
+$("#itemDate").keyup(function (e) {
+    const pattern =/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
+    if (pattern.test($("#itemDate").val())){
+        $("#itemDate").css('border','2px solid green');
+
+
+    }else{
+        $("#itemDate").css('border','2px solid red')
+        if (e.key==='Enter'){
+            console.log(e);
+            e.preventDefault();
+
+        }
+
+    }
+
+});
+
+$("#OPrice").keyup(function (e) {
+    const pattern = /^[0-9]{2,}([.]{1}[0-9]{2})?$/;
+    if (pattern.test($("#OPrice").val())){
+        $("#OPrice").css('border','2px solid green');
+
+
+    }else{
+        $("#OPrice").css('border','2px solid red')
+        if (e.key==='Enter'){
+            console.log(e);
+            e.preventDefault();
+
+        }
+
+    }
+
+});
+
+$("#oQty").keyup(function (e) {
+    const pattern = /^[0-9]{1,}?$/;
+    if (pattern.test($("#oQty").val())){
+        $("#oQty").css('border','2px solid green');
+
+
+    }else{
+        $("#oQty").css('border','2px solid red')
+        if (e.key==='Enter'){
+            console.log(e);
+            e.preventDefault();
+
+        }
+
+    }
+
+});
